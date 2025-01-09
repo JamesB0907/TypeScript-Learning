9f2ff0425571a67da200309aeae2877339fab920
James Brooks <virverum22@gmail.com>
2025-01-08 20:43:30 -0500

    Finished TS for JS programmers

--- TS-for-JS/main.ts
+++ TS-for-JS/main.ts
@@ -135,3 +135,27 @@ const point3 = { x: 12, y: 26, z: 89 };
 logPoint(point3); // logs "12, 26"
 
 const rect = { x: 33, y: 3, width: 30, height: 80 };
+logPoint(rect); // logs "33, 3"
+
+const color = { hex: "#187ABF" };
+// logPoint(color); // Error!
+
+// >> Argument of type '{hex: string;}' is not assigneable to parameter of type 'Point'.
+// >> Type '{hex: string;}' is missing the following properties from type 'Point': x, y.
+
+// We can see from the above example that TypeScript offers more precise error catching than it's ancestor, JavaScript.
+
+    // There is no difference between how classes and objects conform to shapes:
+
+class VirtualPoint { // (Instead of an interface...)
+    x: number;
+    y: number;
+
+    constructor(x: number, y: number) {
+        this.x = x;
+        this.y = y;
+    }
+}
+
+const newVPoint = new VirtualPoint(13, 56);
+logPoint(newVPoint); // logs "13, 56"
